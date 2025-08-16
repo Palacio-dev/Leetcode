838fd36d3b5dcbb2407d6176ead0a65d588de5ae
Palacio <lucaspalacioa@gmail.com>
2025-08-16 11:18:20 -0300

    Mais dois exemplos

---
+++ Array/C.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int lucro = 0;
+        for (int i = 1; i< prices.size(); i++){
+            if (prices[i] > prices[i-1]){
+                lucro += prices[i] - prices[i-1];
+            }
+        }
+        return lucro;
+    }
+};
---
+++ Array/D.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        set<int> dis(nums.begin(),nums.end());
+        return dis.size() < nums.size();
+    }
+};
